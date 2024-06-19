3246e2f507abb19cb170eb547bb4af151d340db0
Lasith Randima <lasithrandima123@gmail.com>
2024-06-19 13:40:03 +0530

    before create controllers

--- src/main/java/com/example/sololearn/SololearnApplication.java
+++ src/main/java/com/example/sololearn/SololearnApplication.java
@@ -1,11 +1,14 @@
 package com.example.sololearn;
 
+import com.example.sololearn.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.awt.*;
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
 
 @SpringBootApplication
@@ -18,14 +21,27 @@ public class SololearnApplication {
 	}
 
 	@GetMapping
-	// returning json
+	// returning json array
 //	public List<String> hello() {
 //		return List.of("Hello", "Sololearn!");
 //	}
 
 	// returning string
-	public String hello() {
-		return "Hello, Sololearn!";
+//	public String hello() {
+//		return "Hello, Sololearn!";
+//	}
+
+//	 returning json array of objects
+	public List<Student> hello() {
+		return List.of(
+				new Student(
+						1L,
+						"Mariam",
+						"mariam.james@gmail.com",
+						LocalDate.of(2000, Month.JANUARY, 5),
+						21
+				)
+		);
 	}
 
 }
---
+++ src/main/java/com/example/sololearn/student/StudentController.java
@@ -0,0 +1,11 @@
+package com.example.sololearn.student;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api/v1/student")
+public class StudentController {
+
+}
